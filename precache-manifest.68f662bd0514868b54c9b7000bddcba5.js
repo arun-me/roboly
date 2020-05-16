17154c2aa1d4248f6caeffb8c08881ca002d7834
arun-me <aruninnovate@pm.me>
2020-05-16 19:56:41 +0530

    Updates

---
+++ precache-manifest.68f662bd0514868b54c9b7000bddcba5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0c3bc26546cc99b639050be8adae92",
+    "url": "/roboly/index.html"
+  },
+  {
+    "revision": "9a93af0fc4170a6960d0",
+    "url": "/roboly/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "87f9a1f5d4f7c028ea28",
+    "url": "/roboly/static/css/main.62195015.chunk.css"
+  },
+  {
+    "revision": "9a93af0fc4170a6960d0",
+    "url": "/roboly/static/js/2.ce0361a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboly/static/js/2.ce0361a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f9a1f5d4f7c028ea28",
+    "url": "/roboly/static/js/main.ae525862.chunk.js"
+  },
+  {
+    "revision": "3169714c49f9e8d7df99",
+    "url": "/roboly/static/js/runtime-main.8b900c0c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboly/static/media/SEGA.12a3db8b.woff"
+  }
+]);
